8ebde0996915d7b49160f5a9d12bafc5e98e8c3c
pasqualino.cristaudo <>
2023-09-20 19:36:10 +0200

    Added listener on internal queue

--- src/main/java/it/pagopa/interop/signalhub/persister/queue/consumer/SqsInternalListener.java
+++ src/main/java/it/pagopa/interop/signalhub/persister/queue/consumer/SqsInternalListener.java
@@ -1,21 +1,54 @@
 package it.pagopa.interop.signalhub.persister.queue.consumer;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import it.pagopa.interop.signalhub.persister.entity.Signal;
+import it.pagopa.interop.signalhub.persister.exception.PdndGenericException;
+import it.pagopa.interop.signalhub.persister.mapper.SignalMapper;
+import it.pagopa.interop.signalhub.persister.queue.model.SignalEvent;
+import it.pagopa.interop.signalhub.persister.service.SignalService;
+import it.pagopa.interop.signalhub.persister.utils.Utility;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
+//import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import static it.pagopa.interop.signalhub.persister.exception.ExceptionTypeEnum.MAPPER_ERROR;
+
 
 @Slf4j
+@Component
 public class SqsInternalListener {
+    @Autowired
+    private SignalService signalService;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private SignalMapper signalMapper;
 
-    public CompletableFuture<Void> listen(String node, @Headers Map<String, Object> headers) {
+    @SqsListener(value = "${aws.internal-queue-name}")
+    public CompletableFuture<Void> pullFromAwsInternalQueue(@Payload String node, @Headers Map<String, Object> headers) {
+        log.info("node: {}, headers: {}, PullFromInternalQueue received input", node, headers);
+        SignalEvent signalEvent = convertToObject(node, SignalEvent.class);
+        Signal signal = signalMapper.signalEventToSignal(signalEvent);
+        signalService
+                .pushIntoAwsDbMaster(signal)
+                .block();
+
         return CompletableFuture.completedFuture(null);
     }
 
+    private <T> T convertToObject(String body, Class<T> tClass){
+        T entity = Utility.jsonToObject(this.objectMapper, body, tClass);
+        if (entity == null) {
+            throw new PdndGenericException(MAPPER_ERROR, MAPPER_ERROR.getMessage());
+        }
+        return entity;
+    }
 }
