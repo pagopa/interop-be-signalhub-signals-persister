5d0b9a69aa434d247dd34d643334fb001edd9959
cristopher.moccia <cristopher.moccia@accenture.com>
2023-10-11 09:35:38 +0200

    refactoring name of column and added new table for tracing batch

--- src/main/resources/db/changelog/changes/init_table/init_table.sql
+++ src/main/resources/db/changelog/changes/init_table/init_table.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS ORGANIZATION_ESERVICE (
+CREATE TABLE IF NOT EXISTS ESERVICE (
     eservice_id     VARCHAR (50) UNIQUE NOT NULL,
-    producer_id VARCHAR (50) NOT NULL,
+    producer_id     VARCHAR (50) NOT NULL,
     state           VARCHAR (50) NOT NULL,
     tmst_insert     TIMESTAMP    NOT NULL,
     tmst_last_edit  TIMESTAMP,
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS CONSUMER_ESERVICE (
 
 CREATE TABLE IF NOT EXISTS SIGNAL (
     id SERIAL PRIMARY KEY,
+    correlation_id VARCHAR(50) NOT NULL,
     signal_id   BIGINT        NOT NULL,
     object_id   VARCHAR (50)  NOT NULL,
     eservice_id VARCHAR (50)  NOT NULL,
@@ -35,17 +36,18 @@ CREATE INDEX SIGNAL_INDEX_SIGNAL_ID ON SIGNAL USING hash (signal_id);
 
 CREATE INDEX SIGNAL_INDEX_ESERVICE_ID ON SIGNAL USING hash (eservice_id);
 
+CREATE INDEX ESERVICE_INDEX_ID ON ESERVICE USING hash (eservice_id);
+CREATE INDEX ESERVICE_INDEX_PRODUCER_ID ON ESERVICE USING hash (producer_id);
 
-CREATE TABLE IF NOT EXISTS EVENT_TEMP (
-    event_temp_id    SERIAL PRIMARY KEY,
-    event_id         BIGINT       UNIQUE NOT NULL,
-    event_type       VARCHAR (50) NOT NULL,
-    object_type      VARCHAR (50) NOT NULL,
-    object_id        VARCHAR (50) NOT NULL,
+CREATE INDEX CONSUMER_ESERVICE_INDEX_ID ON CONSUMER_ESERVICE USING hash (eservice_id);
+CREATE INDEX ESERVICE_INDEX_CONSUMER_ID ON CONSUMER_ESERVICE USING hash (consumer_id);
+
+CREATE TABLE IF NOT EXISTS TRACING_BATCH (
+    batch_id         SERIAL PRIMARY KEY,
     state            VARCHAR (50) NOT NULL,
-    state_processing VARCHAR (50) NOT NULL,
-    tmst_insert      TIMESTAMP    NOT NULL,
-    tmst_processing  TIMESTAMP
+    last_event_id      BIGINT,
+    tmst_started     TIMESTAMP NOT NULL,
+    tmst_ended       TIMESTAMP
 );
 
 
