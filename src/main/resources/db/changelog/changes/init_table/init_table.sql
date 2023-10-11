cea935e3e52acd3a568e1caf05911356581ff7c4
pasqualino.cristaudo <>
2023-10-11 16:58:00 +0200

    Updated init_table's script and h2's schema

--- src/main/resources/db/changelog/changes/init_table/init_table.sql
+++ src/main/resources/db/changelog/changes/init_table/init_table.sql
@@ -6,6 +6,8 @@ CREATE TABLE IF NOT EXISTS ESERVICE (
     tmst_last_edit  TIMESTAMP,
     UNIQUE (eservice_id, producer_id)
 );
+CREATE INDEX ESERVICE_INDEX_ID ON ESERVICE USING hash (eservice_id);
+CREATE INDEX ESERVICE_INDEX_PRODUCER_ID ON ESERVICE USING hash (producer_id);
 
 
 CREATE TABLE IF NOT EXISTS CONSUMER_ESERVICE (
@@ -16,48 +18,42 @@ CREATE TABLE IF NOT EXISTS CONSUMER_ESERVICE (
     tmst_last_edit  TIMESTAMP,
     UNIQUE (eservice_id, consumer_id)
 );
+CREATE INDEX CONSUMER_ESERVICE_INDEX_ID ON CONSUMER_ESERVICE USING hash (eservice_id);
+CREATE INDEX ESERVICE_INDEX_CONSUMER_ID ON CONSUMER_ESERVICE USING hash (consumer_id);
 
 
 CREATE TABLE IF NOT EXISTS SIGNAL (
     id SERIAL PRIMARY KEY,
     correlation_id VARCHAR(50) NOT NULL,
-    signal_id   BIGINT        NOT NULL,
-    object_id   VARCHAR (50)  NOT NULL,
-    eservice_id VARCHAR (50)  NOT NULL,
-    object_type VARCHAR (50)  NOT NULL,
-    signal_type VARCHAR (50)  NOT NULL,
-    tmst_insert TIMESTAMP     NOT NULL,
+    signal_id      BIGINT        NOT NULL,
+    object_id      VARCHAR (50)  NOT NULL,
+    eservice_id    VARCHAR (50)  NOT NULL,
+    object_type    VARCHAR (50)  NOT NULL,
+    signal_type    VARCHAR (50)  NOT NULL,
+    tmst_insert    TIMESTAMP     NOT NULL,
     UNIQUE (signal_id, eservice_id)
 );
-
-
 CREATE INDEX SIGNAL_INDEX_SIGNAL_ID ON SIGNAL USING hash (signal_id);
-
-
 CREATE INDEX SIGNAL_INDEX_ESERVICE_ID ON SIGNAL USING hash (eservice_id);
-
-CREATE INDEX ESERVICE_INDEX_ID ON ESERVICE USING hash (eservice_id);
-CREATE INDEX ESERVICE_INDEX_PRODUCER_ID ON ESERVICE USING hash (producer_id);
-
-CREATE INDEX CONSUMER_ESERVICE_INDEX_ID ON CONSUMER_ESERVICE USING hash (eservice_id);
-CREATE INDEX ESERVICE_INDEX_CONSUMER_ID ON CONSUMER_ESERVICE USING hash (consumer_id);
-
-CREATE TABLE IF NOT EXISTS TRACING_BATCH (
-    batch_id         SERIAL PRIMARY KEY,
-    state            VARCHAR (50) NOT NULL,
-    last_event_id      BIGINT,
-    tmst_started     TIMESTAMP NOT NULL,
-    tmst_ended       TIMESTAMP
-);
 
 
 CREATE TABLE IF NOT EXISTS DEAD_SIGNAL (
     id          SERIAL PRIMARY KEY,
-    signal_id   BIGINT        NOT NULL,
-    object_id   VARCHAR (50)  NOT NULL,
-    eservice_id VARCHAR (50)  NOT NULL,
-    object_type VARCHAR (50)  NOT NULL,
-    signal_type VARCHAR (50)  NOT NULL,
-    tmst_insert TIMESTAMP     NOT NULL,
-    error VARCHAR(255)        NOT NULL
+    correlation_id VARCHAR(50)   NOT NULL,
+    signal_id      BIGINT        NOT NULL,
+    object_id      VARCHAR (50)  NOT NULL,
+    eservice_id    VARCHAR (50)  NOT NULL,
+    object_type    VARCHAR (50)  NOT NULL,
+    signal_type    VARCHAR (50)  NOT NULL,
+    tmst_insert    TIMESTAMP     NOT NULL,
+    error_reason   VARCHAR(255)  NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS TRACING_BATCH (
+     batch_id         SERIAL PRIMARY KEY,
+     state            VARCHAR (50) NOT NULL,
+     last_event_id      BIGINT,
+     tmst_started     TIMESTAMP NOT NULL,
+     tmst_ended       TIMESTAMP
 );
